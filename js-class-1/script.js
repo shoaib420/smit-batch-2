bd47610954fa201343d7be54802fac5930fd8e21
shoaib soomro <64595451+shoaib420@users.noreply.github.com>
2025-08-20 04:38:45 -0700

    js-class-1

---
+++ js-class-1/script.js
@@ -0,0 +1,11 @@
+// console.log("hello world ")
+
+// variable 
+console.log(name_1);
+var  Name =  "shahid ";
+
+var name_1 = "Wasif "
+Name = "IKRAM";
+
+// console.log(name_1);
+console.log(Name)
