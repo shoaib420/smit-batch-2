6c20b1e19a77c9067752b00bc51b590a5a172ffc
shoaib soomro <64595451+shoaib420@users.noreply.github.com>
2025-08-21 04:10:46 -0700

    JS-CLASS-2

---
+++ js-class-2/script.js
@@ -0,0 +1,30 @@
+var usrName = "shahid";
+console.log(usrName)
+ usrName  = "abdullah"
+ usrName = "ikram";
+ console.log(usrName)
+ usrName = "wasif"
+ console.log(usrName)
+ usrName = "ZEESHAN" ;
+
+ let userNumber = 123456;
+
+ userNumber = 577576547;
+ userNumber = 8700000;
+console.log(userNumber)
+
+let name = "javeed "
+name = "salman";
+console.log(name)
+name = "iqbal"
+console.log(name )
+
+//  const userNumber_1 = 87654;
+//   console.log(userNumber_1)
+//  userNumber_1 = 65432
+//  console.log(userNumber_1)
+
+const Name_2 = "hussan "
+Name_2 = "ikram";
+Name_2 = "shoaib";
+console.log(Name_2)
