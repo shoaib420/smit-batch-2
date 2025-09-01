3a76114c83c959b18f09dba9d77c508fb6f7dafc
shoaib soomro <64595451+shoaib420@users.noreply.github.com>
2025-09-01 04:21:58 -0700

    CLASS-5

---
+++ class-5-js/App.js
@@ -0,0 +1,43 @@
+// comparison operator
+// == , === , < , > 
+
+// var a = 20;
+// var b = "20"; 
+// console.log(a == b);
+
+
+// var num = 33;
+// var num2 = 43;
+// var c = num < num2;
+// console.log(c)
+
+// var number = 45;
+// var number2 = 44;
+// console.log(number > number2)
+``
+// var val = 88;
+// var val2 = 99;
+// var a = val != val2;
+// console.log(a)
+
+// var val = 88;
+// var val2 = 99;
+// var a = val !== val2;
+// console.log(a)
+
+// logical operator
+// var a = 33;
+// var b = 44;
+// console.log(a  b)
+
+// 1. logical and (&&);
+// T && T => TRUE;
+// T && F => FALSE;
+// F && T => FALSE;
+// F && F => FALSE;
+
+// 2 . LOGICAL OR (||);
+// T || T => TRUE;
+// T || F => TRUE;
+// F || T => TRUE;
+// F || F => FALSE;
