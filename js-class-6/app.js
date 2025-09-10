c065a5b782a7728ebb0679ace5b602a149080623
shoaib soomro <64595451+shoaib420@users.noreply.github.com>
2025-09-10 04:37:26 -0700

    if else condition

---
+++ js-class-6/app.js
@@ -0,0 +1,44 @@
+// console.log("hello world ")
+//  var name = false ? "shahid" : "ali";
+//  console.log(name);
+
+// var num = 10 % 2 === 0;
+// console.log(num)
+
+// var odd = 10 % 2 !== 0;
+// console.log(odd)
+// console.log("hello world 1 ")
+// {
+//     console.log("hello world 2 block ")
+// }
+
+// console.log("hello 3")
+// if (2 + 9 -1 == 12){
+//     console.log("hello world block")
+// }else(
+//     console.log("code is error ")
+// )
+
+// var userNum = prompt("inter your number ");
+// if(userNum == 0){
+//     console.log("num is zero   ")
+// }
+// if(userNum % 2 == 0){
+//     console.log("even number ")
+// }else{
+//     console.log("odd num")
+// }
+
+var student = prompt("inter your marks ");
+
+if(student >= 80){
+    console.log("Grade A ")
+}else if(student >= 70){
+    console.log(console.log("grade b"))
+}else if(student >= 60){
+    console.log("grade c ")
+}else if(student >= 50){
+    console.log("grade D ")
+}else{
+    console.log("student F")
+}
