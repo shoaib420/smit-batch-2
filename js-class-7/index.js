aeae55ee83fc7faa87b8e3cdf842064a011f061f
shoaib soomro <64595451+shoaib420@users.noreply.github.com>
2025-09-30 21:59:58 -0700

    last-commit

---
+++ js-class-7/index.js
@@ -0,0 +1,23 @@
+ var number  = prompt("inter your number ");
+
+ switch (number) {
+    case "1" : {
+        console.log("monday")
+        break;
+    }
+    case "2" : {
+        console.log("tue")
+        break;
+    }
+    case "   " :{
+        console.log("wen");
+        break;
+    }
+        
+       
+ 
+    default: {
+        console.log("invalid your number")
+    }
+        break;
+ }
---
+++ js-class-8/app.js
@@ -0,0 +1,64 @@
+// loop
+// console.log("hello world ")
+// console.log("hello world ")
+// console.log("hello world ")
+// console.log("hello world ")
+// console.log("hello world ")
+// console.log("hello world ")
+// console.log("hello world ")
+// console.log("hello world ") 
+// console.log("hello world ") 
+// console.log("hello world ")
+
+//while loop
+// var count = 0;
+// while(count < 100){
+//     console.log("hello world ", count)
+//     count++
+// } 
+
+// var count = 1;
+// while(count <= 10){
+//     console.log(` 2 * ${count} =  ${2 * count}`)
+//     count++
+// } 
+// console.log("hello world ")
+// // var i = 30;
+// while(i < 20 ){
+//     console.log(i)
+//     i = i + 2 
+// do while loop
+// do while loop
+// do while loop
+
+// var i = 30;
+// do{
+//     console.log(i)
+//     i = i + 10
+// }while(1 > 10)
+// var sum = 0;
+// for(i = 1; i <= 100; i++){
+//     // console.log(i)
+//     sum = sum + i
+// }
+// console.log(sum)
+
+// var sum = 0;
+// for(i = 1; i <= 100; i++){
+//  if(i % 2 != 0){
+//         sum = sum + i
+//  }
+
+// }
+// console.log(sum)
+abc() // function calling 
+console.log("line 1 ")
+console.log("line 2 ")
+console.log("line 3 ")
+
+console.log("line 5 ")
+
+function abc(){
+    console.log("line 4 ")
+    console.log("line 6 ")
+}
